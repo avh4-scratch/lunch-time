d7317b5ec78ce463c8027e45027dc845e5e56104
Aaron VonderHaar and Tyler Schultz <pair+av+tyler@pivotallabs.com>
2014-01-29 14:19:24 -0800

    Remove JDBC and unnecessary thymeleaf dependencies

--- src/main/java/com/pivotallabs/lunchtime/Application.java
+++ src/main/java/com/pivotallabs/lunchtime/Application.java
@@ -2,28 +2,21 @@ package com.pivotallabs.lunchtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.sql.DataSource;
-
-import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
 @ComponentScan
 @EnableAutoConfiguration
-@EnableJpaRepositories
+//@EnableJpaRepositories
 @EnableWebMvc
 public class Application {
-
-    @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(H2)
-                .build();
-    }
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        return new EmbeddedDatabaseBuilder()
+//                .setType(H2)
+//                .build();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/test/java/com/pivotallabs/lunchtime/integration/IntegrationTestBase.java
+++ src/test/java/com/pivotallabs/lunchtime/integration/IntegrationTestBase.java
@@ -4,35 +4,32 @@ import com.pivotallabs.lunchtime.Application;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.inject.Inject;
-import javax.sql.DataSource;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @SpringApplicationConfiguration(classes = Application.class)
 public class IntegrationTestBase {
     protected MockMvc mockMvc;
-    protected JdbcTemplate jdbcTemplate;
-    @Inject private DataSource dataSource;
+//    protected JdbcTemplate jdbcTemplate;
+//    @Inject private DataSource dataSource;
     @Inject private WebApplicationContext wac;
 
     @Before
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.<StandaloneMockMvcBuilder>webAppContextSetup(wac).build();
-        jdbcTemplate = new JdbcTemplate(dataSource);
+//        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     protected int countRowsInTable(String tableName) {
-        return JdbcTestUtils.countRowsInTable(jdbcTemplate, tableName);
+//        return JdbcTestUtils.countRowsInTable(jdbcTemplate, tableName);
+        return -1;
     }
 }
