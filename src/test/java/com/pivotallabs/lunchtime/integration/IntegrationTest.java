330ff133056cd3858cca57ecd4bdf51c25d12ed4
Aaron VonderHaar and Tyler Schultz <pair+av+tyler@pivotallabs.com>
2014-01-29 14:22:08 -0800

    index shows add person text

--- src/test/java/com/pivotallabs/lunchtime/integration/IntegrationTest.java
+++ src/test/java/com/pivotallabs/lunchtime/integration/IntegrationTest.java
@@ -1,14 +1,11 @@
 package com.pivotallabs.lunchtime.integration;
 
 import org.junit.Test;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
 public class IntegrationTest extends IntegrationTestBase {
 
@@ -24,7 +21,7 @@ public class IntegrationTest extends IntegrationTestBase {
     public void personIndex_shouldHaveFormToAddAPerson() throws Exception {
         mockMvc.perform(get("/person"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Add person"));
+                .andExpect(content().string(containsString("Add person")));
     }
 
 }
